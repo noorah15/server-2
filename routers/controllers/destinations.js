987e8491acd636bd128e2e468b889ce28a6f95af
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-25 21:40:18 +0300

    edit destinations route & cotroller

--- routers/controllers/destinations.js
+++ routers/controllers/destinations.js
@@ -45,6 +45,54 @@ const getDestinations = (req, res) => {
     });
 };
 
+const getDestinationById = (req, res) => {
+  const { id } = req.params;
+  destinations
+    .findOne({ _id: id, isDel: false })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const getDestinationByCatg = (req, res) => {
+  const { catg } = req.params;
+  destinations
+    .findOne({ catg, isDel: false })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const getDestinationByDays = (req, res) => {
+  const { days } = req.params;
+  destinations
+    .findOne({ days, isDel: false })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const getDestinationByCity = (req, res) => {
+  const { city } = req.params;
+  destinations
+    .findOne({ city, isDel: false })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
 const delDestination = async (req, res) => {
   const { destinationId } = req.body;
 
@@ -52,8 +100,16 @@ const delDestination = async (req, res) => {
     let doc = await hotels.updateOne({ _id: destinationId }, { isDel: true });
     res.status(200).json(doc);
   } catch (err) {
-    res.status(400).json("Post not found");
+    res.status(400).json(err);
   }
 };
 
-module.exports = { addDestinations, getDestinations, delDestination };
+module.exports = {
+  addDestinations,
+  getDestinations,
+  getDestinationById,
+  getDestinationByCatg,
+  getDestinationByDays,
+  getDestinationByCity,
+  delDestination,
+};
--- routers/routes/destinations.js
+++ routers/routes/destinations.js
@@ -2,12 +2,20 @@ const express = require("express");
 const {
   addDestinations,
   getDestinations,
+  getDestinationById,
+  getDestinationByCatg,
+  getDestinationByDays,
+  getDestinationByCity,
   delDestination,
 } = require("./../controllers/destinations.js");
 const destinations = express.Router();
 
 destinations.post("/add", addDestinations);
 destinations.get("/get", getDestinations);
+destinations.get("/getDestinationById/:id", getDestinationById);
+destinations.get("/getDestinationByCatg/:catg", getDestinationByCatg);
+destinations.get("/getDestinationByDays/:days", getDestinationByDays);
+destinations.get("/getDestinationByCity/:city", getDestinationByCity);
 destinations.delete("/del", delDestination);
 
 module.exports = destinations;
