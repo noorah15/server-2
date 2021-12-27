be6055a10c02f451270d490681ad97c32bfe7608
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-27 14:40:42 +0300

    add updateTouristGuides route & controller

--- routers/routes/touristGuides.js
+++ routers/routes/touristGuides.js
@@ -3,10 +3,12 @@ const {
   addTouristGuides,
   getTouristGuides,
   delTouristGuides,
+  updateTouristGuides,
 } = require("./../controllers/touristGuides.js");
 const touristGuides = express.Router();
 
 touristGuides.post("/add", addTouristGuides);
+touristGuides.put("/update", updateTouristGuides);
 touristGuides.get("/get", getTouristGuides);
 touristGuides.delete("/del", delTouristGuides);
 
