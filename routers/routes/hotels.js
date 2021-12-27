df710a50ccdaaf55e773359a11d7471867d166fa
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-27 14:34:57 +0300

    add updateHotels route & controller

--- routers/routes/hotels.js
+++ routers/routes/hotels.js
@@ -3,10 +3,12 @@ const {
   addHotels,
   getHotels,
   delHotel,
+  updateHotels,
 } = require("./../controllers/hotels.js");
 const hotels = express.Router();
 
 hotels.post("/add", addHotels);
+hotels.put("/update", updateHotels);
 hotels.get("/get", getHotels);
 hotels.delete("/del", delHotel);
 
